03e5f50f4abff5a1ac18b3d13aa61a0a1f5373bf
jonathanavibiswas10 <YOUR_NOREPLY_EMAIL>
2026-08-27 16:36:19 +0600

    Add contact section

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=script.js.map
+
+const navBar = document.querySelector(".nav-bar");
+
+const links = navBar.querySelectorAll("a");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        console.log(link.textContent);
+    });
+});
